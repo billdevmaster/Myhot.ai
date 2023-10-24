ad978854f708443ced295faffb96a9748cba282c
billdevmaster71 <billdevmaster@gmail.com>
2023-10-24 11:56:45 +0300

    fix: socket url

--- web/store/socket.ts
+++ web/store/socket.ts
@@ -16,7 +16,7 @@ createSocket()
 
 function createSocket() {
   const socketUrl = baseUrl.replace('https://', 'wss://').replace('http://', 'ws://')
-  const ws = new WebSocket(socketUrl)
+  const ws = new WebSocket(socketUrl + "api/")
 
   socket = ws
   ws.onopen = onConnected
