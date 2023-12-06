16e7c3fdf26b38ca561dedbef447997baac16abb
billdevmaster71 <billdevmaster@gmail.com>
2023-12-06 07:39:56 +0200

    fix: update for ipaddress

--- srv/api/chat/get.ts
+++ srv/api/chat/get.ts
@@ -52,7 +52,7 @@ function getCharacterIds(chats: Document[]) {
 }
 
 export const getChat = handle(async (req) => {
-  const clientIP = req.headers['X-Real-IP'];
+  const clientIP = req.headers['x-real-ip'];
   console.log('Client IP Address:', clientIP);
   const userId = req.body.userId
   const charId = req.body.charId
