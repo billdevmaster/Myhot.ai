514c11e489959083c1ab8f4b3c64b11a3c53a808
billdevmaster71 <billdevmaster@gmail.com>
2023-12-13 18:39:06 +0200

    fix: update for chat detail responsive

--- web/pages/Chat/ChatDetail.tsx
+++ web/pages/Chat/ChatDetail.tsx
@@ -361,7 +361,7 @@ const ChatDetail: Component = () => {
             </header>
             
             <section
-              class={`chat-container max-w-7xl m-auto w-full h-full gap-1 overflow-y-auto ${msgsAndPaneJustifyContent()}`}
+              class={`max-w-7xl m-auto w-full h-full gap-1 overflow-y-auto ${msgsAndPaneJustifyContent()}`}
             >
               <section class="flex h-full w-full flex-col justify-between gap-2">
                 <div class="flex flex-col sm:flex-row overflow-y-auto h-full justify-between">
